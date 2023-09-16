74de2bb9a56414dc2ca974d0896faab74324521f
hotcat190 <nguyendinhnguyen151204@gmail.com>
2023-09-16 15:00:30 +0700

    change action.py

--- action.py
+++ action.py
@@ -66,52 +66,52 @@ def attack(driver, auto_open_captcha):
 def loot(driver):
     try:
         action = driver.find_element(By.XPATH, '//button[@class="inline-flex items-center px-3 py-2 border border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"]')
-        action.click()
-        print("Material found!")
-        craft = driver.find_element(By.XPATH, '//button[@id="crafting_button"]')
-        print("Looting...")
-        while not craft.text.strip() == "Press here to close":
-            craft.click()  
-            time.sleep(0.5)
-        print("Material looted!")
-        print()
-        craft.click()
-        time.sleep(2)
-        return True
     except:
         return False
+    action.click()
+    print("Material found!")
+    craft = driver.find_element(By.XPATH, '//button[@id="crafting_button"]')
+    print("Looting...")
+    while not craft.text.strip() == "Press here to close":
+        craft.click()
+        time.sleep(0.5)
+    print("Material looted!")
+    print()
+    craft.click()
+    time.sleep(2)
+    return True
 
 def step(driver):
     try:
         step_button = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.XPATH, '(//button[starts-with(@id, "step_btn_")])[3]'))
         )
-        if step_button.is_enabled():
-            step_button.click()
-            time.sleep(1)
-        else:
-            time.sleep(3)
-            step_button.click()
-        return True
     except:
         return False
+    if step_button.is_enabled():
+        step_button.click()
+        time.sleep(1)
+    else:
+        time.sleep(3)
+        step_button.click()
+    return True
 
 def item_check(driver):
     try:
         found_item = driver.find_element(By.XPATH, "//*[text()='You have found an item!']")
-        if found_item.text.strip() == "You have found an item!":
-            print("You found an item!")
-            rand_str = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
-            file_name = (f'item_{rand_str}.png')
-            print(f"Item found! Don't forget to check your inventory. Screenshot saved.")
-            folder_path = os.path.join(os.getcwd(), 'pages/items')
-            if not os.path.exists(folder_path):
-                os.makedirs(folder_path)
-            file_path = os.path.join(folder_path, file_name)
-            driver.save_screenshot(file_path)
-        return True
     except:
         return False
+    if found_item.text.strip() == "You have found an item!":
+        print("You found an item!")
+        rand_str = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+        file_name = (f'item_{rand_str}.png')
+        print(f"Item found! Don't forget to check your inventory. Screenshot saved.")
+        folder_path = os.path.join(os.getcwd(), 'pages/items')
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        file_path = os.path.join(folder_path, file_name)
+        driver.save_screenshot(file_path)
+        return True
 
 # open new tab with link containing element specified in argument
 # not in use
