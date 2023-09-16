5ec0001ee7b8d16b22c28cfc716d08c7cfef0131
hotcat190 <nguyendinhnguyen151204@gmail.com>
2023-09-16 13:29:07 +0700

    save a screenshot when an exception occure

--- umiyuri.py
+++ umiyuri.py
@@ -85,6 +85,8 @@ def main():
             print()
             prRed("An error occurred: ")
             print(error)
+            print("Saving a screenshot of the browser when the exception occurred")
+            driver.save_screeshot("screenshot.png")
             print("quiting driver from exception")
             driver.quit()
             return
